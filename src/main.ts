a7bbad9451d3444155884779e07fb349122073ca
poicxz <zhangxiaoqian96@126.com>
2022-12-08 15:29:11 +0800

    dist

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import Vant from 'vant';
+import 'vant/lib/index.css';
+const htmlDOM = document.getElementsByTagName('html')[0]
+htmlDOM.style.fontSize = ((screen.width / 750) * 16).toFixed(0) + 'px'
+
+createApp(App).use(router,Vant).mount('#app')
